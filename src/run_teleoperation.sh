d3a09e741df075ed3980f6b2615c4f3f7a45795d
jeokim75 <54966534+jeokim75@users.noreply.github.com>
2019-11-21 12:09:39 +0900

    Update run_teleoperation.sh

--- src/run_teleoperation.sh
+++ src/run_teleoperation.sh
@@ -3,7 +3,7 @@
 set -e
 source /opt/ros/dashing/setup.bash
 source /turtlebot3_ws/install/setup.bash
-export ROS_DOMAIN_ID=40
+export ROS_DOMAIN_ID=30
 export TURTLEBOT3_MODEL=burger
 
 echo "========================================"
